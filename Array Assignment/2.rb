5d55807242a19edecd882113a0d5edb0ba8b0013
Sanket Panasuriya <sanket.panasuriya@bacancy.com>
2022-03-02 12:18:06 +0000

    Exercise 2 updated with exaplaination

--- Array Assignment/2.rb
+++ Array Assignment/2.rb
@@ -5,9 +5,17 @@ Given a number n, return true if n is palindrome.
 =end
 
 def is_palindrome?(str)
+  ## Converting 'str' to String
   str = str.to_s
+  
+  
+  ## return true if 'str' length is 1 
   return true if str.length <= 1
+  
+  ## return false if first and last digit is not same 
   return false if !(/(\d)(\d*)\1$/.match?(str))
+  
+  ## sending to recursive manner with removing first and last charecter
   is_palindrome?(str[1...-1])
 end
 
@@ -15,17 +23,26 @@ end
 ## Second Way
 
 def check_palindrome(string)
-  string = string.to_s if string.is_a? Integer     
+
+  ## Check where 'string' is Interger if Integer then convert it to String
+  string = string.to_s if string.is_a? Integer 
+  
+  ## Storing 'string' into 'str'
   str = string
+  
   count = string.length / 2
   
+  ## Continue loop until count is not equal 0
   while count != 0
+    
+    ## Check Regex is match with 'str' if match then remove fist and last charcter in to 'str' if not then return false
     if /(\d)(\d*)\1$/.match?(str) 
       str = str[1...-1]
     else 
       return false
     end
     
+    ## Incresing Count
     count -= 1
   end
   return true
