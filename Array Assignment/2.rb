5653a0aec84aa835edd83058d0937c9e0b59713b
Sanket Panasuriya <sanket.panasuriya@bacancy.com>
2022-03-02 09:07:32 +0000

    Exercise 2 Done

---
+++ Array Assignment/2.rb
@@ -0,0 +1,44 @@
+=begin
+
+Given a number n, return true if n is palindrome.
+
+=end
+
+def is_palindrome?(str)
+  str = str.to_s
+  return true if str.length <= 1
+  return false if !(/(\d)(\d*)\1$/.match?(str))
+  is_palindrome?(str[1...-1])
+end
+
+
+## Second Way
+
+def check_palindrome(string)
+  string = string.to_s if string.is_a? Integer     
+  str = string
+  count = string.length / 2
+  
+  while count != 0
+    if /(\d)(\d*)\1$/.match?(str) 
+      str = str[1...-1]
+    else 
+      return false
+    end
+    
+    count -= 1
+  end
+  return true
+end
+
+
+print "Enter Number: "
+str = gets.chomp
+
+puts check_palindrome(str)
+
+puts is_palindrome?(str)
+
+## Simplest way to find Palindrome is below but it's not optimum solution 
+## Just because of whet is the input is '123 321' this is not like Palindrome but generate get true answer
+puts str == str.reverse
