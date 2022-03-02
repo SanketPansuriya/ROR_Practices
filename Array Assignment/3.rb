fe424e9bf37ea5862333e0cacb5e2973cbb80ee2
Sanket Panasuriya <sanket.panasuriya@bacancy.com>
2022-03-02 12:18:14 +0000

    Exercise 3 updated with exaplaination

--- Array Assignment/3.rb
+++ Array Assignment/3.rb
@@ -7,13 +7,22 @@ Expected output: [0,1,2,3,4, nil, nil, nil, nil, nil]
 =end
 
 def get_uniq(arr)
+  ## empty array 'b' for storing output
   b = []
+  
+  ## Integer variable for tracking index of 'b'
   index = 0
+  
+  ## Iterete loop in 'arr' array
   arr.each do |v| 
+    
+    ## Checking Where 'b' array has 'v' value or not 
+    ## if 'v' value is there then push nil at last position into 'b'
     if b.include?(v)
       next if b.push(nil)
     end
     
+    ## Pushing 'v' value at 'index' position
     b.insert(index, v)
     index +=1
   end
